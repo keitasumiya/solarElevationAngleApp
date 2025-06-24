60f03ae127700f192f6a51d64bb5fc2bfcbfea84
Keita Sumiya <ksumiya0318@gmail.com>
2025-06-24 16:18:27 +0900

    change port

--- solar-app/app.py
+++ solar-app/app.py
@@ -8,6 +8,7 @@ import base64
 import pytz
 import numpy as np
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
@@ -166,4 +167,5 @@ def export_csv():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
